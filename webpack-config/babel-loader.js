66897562b81ed21c7040a16d250e00768bb6f920
Yuri Skurikhin <yunixon@gmail.com>
2016-10-19 10:08:26 +0300

    Add jquery loader

--- webpack-config/babel-loader.js
+++ webpack-config/babel-loader.js
@@ -6,10 +6,6 @@ module.exports = function () {
           test: /\.jsx?$/,
           loader: 'babel?presets[]=es2015&presets[]=stage-2&presets[]=react',
           exclude: /node_modules/,
-        },
-        {
-          test: /bootstrap.+\.(jsx|js)$/,
-          loader: 'imports?jQuery=jquery,$=jquery,this=>window'
         }
       ]
     }
