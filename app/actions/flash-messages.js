bfdce6526cc3c5685bfa667e105ff28b775b3234
Ildar Shaynurov <shaynurov@gmail.com>
2016-10-21 17:12:48 +0300

    Refactor flash messages

--- app/actions/flash-messages.js
+++ app/actions/flash-messages.js
@@ -1,32 +1,10 @@
+import { createAction } from 'redux-actions'
 import uuid from 'uuid'
-
-import * as types from 'constants/flash-message-action-types'
 
 const defaultOptions = {
   type: 'success',
   autoClose: true,
 }
 
-
-export function addFlashMessage(text, options = {}) {
-  return {
-    type: types.FLASH_MESSAGE_ADD,
-    payload: {
-      message: {
-        ...defaultOptions,
-        uid:  uuid.v4(),
-        text: text,
-        ...options,
-      }
-    }
-  }
-}
-
-export function removeFlashMessage(message_uid) {
-  return {
-    type: types.FLASH_MESSAGE_REMOVE,
-    payload: {
-      message_uid: message_uid,
-    }
-  }
-}
+export const addFlashMessage    = createAction('FLASH_MESSAGE_ADD',    (text, options = {}) => ({ message: {...defaultOptions, uid: uuid.v4(), text: text, ...options } }))
+export const removeFlashMessage = createAction('FLASH_MESSAGE_REMOVE', (messageUID) => ({ messageUID }))
--- app/reducers/flash-messages.js
+++ app/reducers/flash-messages.js
@@ -1,26 +1,21 @@
-import * as types from 'constants/flash-message-action-types'
+import { handleActions } from 'redux-actions'
 
-const initialState = {
+import { addFlashMessage, removeFlashMessage } from 'actions'
+
+const defaultState = {
   messages: []
 }
 
-
-export default (state = initialState, action) => {
-  switch(action.type) {
-    case types.FLASH_MESSAGE_ADD:
-      return {
-        ...state,
-        messages: [
-          action.payload.message,
-          ...state.messages,
-        ]
-      }
-    case types.FLASH_MESSAGE_REMOVE:
-      return {
-        ...state,
-        messages: state.messages.filter((m) => m.uid != action.payload.message_uid)
-      }
-    default:
-      return state
-  }
-}
+export default handleActions({
+  [addFlashMessage]: (state, { payload }) => ({
+    ...state,
+    messages: [
+      payload.message,
+      ...state.messages,
+    ]
+  }),
+  [removeFlashMessage]: (state, { payload }) => ({
+    ...state,
+    messages: state.messages.filter(m => m.uid != payload.messageUID)
+  }),
+}, defaultState)
