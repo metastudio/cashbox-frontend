831cc44d4b8c36c49d1f89670b6693c61248c8b7
Ildar Shaynurov <shaynurov@gmail.com>
2018-03-27 15:17:28 +0300

    add font-awesome

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import registerServiceWorker from 'registerServiceWorker';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
+import 'font-awesome/css/font-awesome.css';
 import './index.css';
 
 ReactDOM.render(
