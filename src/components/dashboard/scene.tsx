d4fb2500731ca47d42ae2f64fa4299f738705dbc
Ildar Shaynurov <shaynurov@gmail.com>
2018-03-27 14:56:58 +0300

    Fix dashboard

--- src/components/dashboard/scene.tsx
+++ src/components/dashboard/scene.tsx
@@ -1,18 +1,16 @@
 import * as React from 'react';
-import { Route } from 'react-router-dom';
 
 import RequireLogin from 'components/utils/require-login';
 import RequireOrganization from 'components/require-organization';
 import MainLayout from 'components/layouts/main-layout';
 
-import Transactions from 'components/transactions';
+import Transactions from 'components/transactions/transactions.jsx';
 
 const DashboardScene: React.SFC<{}> = ({ children }) => (
   <RequireLogin>
     <RequireOrganization>
       <MainLayout>
         <Transactions />
-        <Route path="/transactions/new" component={ Transactions.New } />
       </MainLayout>
     </RequireOrganization>
   </RequireLogin>
