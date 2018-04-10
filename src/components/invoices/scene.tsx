106cfcb834e1e67df800edc155f4b8ac6e0f7466
Ildar Shaynurov <shaynurov@gmail.com>
2018-04-10 17:13:06 +0300

    fix(Invoices): fix routes

--- src/components/invoices/scene.tsx
+++ src/components/invoices/scene.tsx
@@ -1,21 +1,23 @@
 import * as React from 'react';
-import { Route } from 'react-router';
+import { Route, Switch } from 'react-router';
 
 import RequireLogin from 'components/utils/require-login';
 import MainLayout from 'components/layouts/main-layout';
 
 import List from './list';
-import New from './new.jsx';
+import New from './new';
 import Show from './show';
 import Edit from './edit';
 
 const InvoicesScene: React.SFC<{}> = () => (
   <RequireLogin>
     <MainLayout>
-      <Route exact path="/invoices" component={ List } />
-      <Route path="/invoices/new" component={ New } />
-      <Route path="/invoices/:id/edit" component={ Edit } />
-      <Route exact path="/invoices/:id" component={ Show } />
+      <Switch>
+        <Route exact path="/invoices" component={ List } />
+        <Route path="/invoices/new" component={ New } />
+        <Route exact path="/invoices/:id" component={ Show } />
+        <Route path="/invoices/:id/edit" component={ Edit } />
+      </Switch>
     </MainLayout>
   </RequireLogin>
 );
