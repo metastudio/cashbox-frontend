aa1cb8ec429ba26ba7a90df5e280b7d658c15675
Yuri Skurikhin <yunixon@gmail.com>
2018-04-12 10:39:03 +0300

    cleanup categories api

--- src/api/categories.js
+++ src/api/categories.js
@@ -1,9 +1,9 @@
 import { prepareURL, getApi, postApi, putApi, deleteApi } from './_helpers';
 
-const categoriesURL = (orgId, params) => prepareURL(`/api/organizations/${orgId}/categories`, params);
+const categoriesURL = (orgId, params) => prepareURL(`/api/organizations/${orgId}/categories`);
 const categoryURL = (orgId, categoryId) => prepareURL(`/api/organizations/${orgId}/categories/${categoryId}`);
 
-export const getOrganizationCategories  = (orgId, params) => getApi(categoriesURL(orgId, params));
+export const getOrganizationCategories  = (orgId) => getApi(categoriesURL(orgId));
 export const getOrganizationCategory    = (orgId, categoryId) => getApi(categoryURL(orgId, categoryId));
 export const postOrganizationCategory   = (orgId, data) => postApi(categoriesURL(orgId), { category: data });
 export const putOrganizationCategory    = (orgId, categoryId, data) => putApi(categoryURL(orgId, categoryId), { category: data });
