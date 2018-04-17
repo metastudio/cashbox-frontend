232f17c2bf1816e5f74fe9db1dfc581225c38993
Ildar Shaynurov <shaynurov@gmail.com>
2018-04-17 11:44:50 +0300

    refact: money formatter

--- src/utils/money/money.ts
+++ src/utils/money/money.ts
@@ -7,31 +7,32 @@ interface Money {
   };
 }
 
-// I found that money format looks better if you use native locale for currency
-const localeForCurrency = (isoCode: string): string | undefined => {
-  switch (isoCode) {
-    case 'USD':
-    case 'EUR':
-      return 'en-US';
-    case 'RUB':
-      return 'ru-RU';
-    default:
-      return undefined;
-  }
+const Formatters = {
+  'USD': new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    currencyDisplay: 'symbol',
+  }),
+  'RUB': new Intl.NumberFormat('ru-RU', {
+    style: 'currency',
+    currency: 'RUB',
+    currencyDisplay: 'symbol',
+  }),
+  'EUR': new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'EUR',
+    currencyDisplay: 'symbol',
+  }),
 };
 
-const formatMoney = (money?: Money): string | null => {
-  if (!money) { return null; }
+const formatMoney = (money?: Money): string | undefined => {
+  if (!money) { return undefined; }
+  const formatter = Formatters[money.currency.isoCode];
+  if (!formatter) { return undefined; }
 
   const float = Number(money.fractional) / money.currency.subunitToUnit;
-  return float.toLocaleString(
-    localeForCurrency(money.currency.isoCode),
-    {
-      style: 'currency',
-      currency: money.currency.isoCode,
-      currencyDisplay: 'symbol',
-    }
-  );
+
+  return formatter.format(float);
 };
 
 export { Money, formatMoney };
