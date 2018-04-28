bdea59e291adbf14d3f8a3f2503eb8c38d0b7588
Yuri Skurikhin <yunixon@gmail.com>
2018-04-28 15:17:39 +0300

    cleanup auth flow

--- src/components/require-organization/require-organization.jsx
+++ src/components/require-organization/require-organization.jsx
@@ -2,19 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
-import { getCookies } from 'utils/cookies';
 
 import { addFlashMessage } from 'actions/flash-messages.js';
-import { clearCurrentOrganization } from 'actions/organizations.js';
 import { getHasCurrentOrganization } from 'selectors/organizations.js';
 
 class RequireOrganization extends React.Component {
   componentDidMount() {
-    if (this.props.userId !== getCookies().userId) {
-      this.props.clearCurrentOrganization();
-    } else {
-      this.checkOrganization(this.props);
-    }
+    this.checkOrganization(this.props);
   }
 
   componentWillReceiveProps(props) {
@@ -37,20 +31,16 @@ class RequireOrganization extends React.Component {
 }
 
 RequireOrganization.propTypes = {
-  userId:                   PropTypes.number.isRequired,
-  clearCurrentOrganization: PropTypes.func.isRequired,
   hasOrganization:          PropTypes.bool.isRequired,
   addFlashMessage:          PropTypes.func.isRequired,
   children:                 PropTypes.node,
 };
 
 const select = (state) => ({
-  userId:          state.auth.user.id,
   hasOrganization: getHasCurrentOrganization(state),
 });
 
 const dispatches = (dispatch) => ({
-  clearCurrentOrganization: () => dispatch(clearCurrentOrganization()),
   addFlashMessage:          (message, options = {}) => dispatch(addFlashMessage(message, options)),
 });
 
--- src/sagas/auth.js
+++ src/sagas/auth.js
@@ -11,6 +11,8 @@ import {
   logoutUser,
 } from 'actions/auth.js';
 
+import { clearCurrentOrganization } from 'actions/organizations.js';
+
 function* handleRestoreSession({ meta: { resolve, reject } }) {
   try {
     yield put(restoreSession.request());
@@ -40,6 +42,10 @@ function* handleLoginUser({ payload: { email, password }, meta: { resolve, rejec
     const token = yield call(postToken, email || '', password);
     setCookies({ token: token });
     const user = yield call(getCurrentUser);
+    if (user.id !== getCookies().userId) {
+      setCookies({ currentOrganizationId: undefined });
+      yield put(clearCurrentOrganization());
+    }
     yield put(loginUser.success(email, token, user));
     yield call(resolve, user);
   } catch (error) {
