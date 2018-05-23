2a04997cc11b9dccf70e630f19354a0570a134ae
Ildar Shaynurov <shaynurov@gmail.com>
2018-05-23 11:08:50 +0300

    feat(graphql): rewrite categories list via graphql

--- src/components/categories/list/categories.tsx
+++ src/components/categories/list/categories.tsx
@@ -1,73 +1,64 @@
 import * as React from 'react';
+import { connect } from 'react-redux';
+import { Alert, Table } from 'react-bootstrap';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
 
-import { connect, Dispatch } from 'react-redux';
-import { Table } from 'react-bootstrap';
-
-import { Status, Category } from 'model-types';
-import { loadCategories } from 'actions/categories.js';
 import { getCurrentOrganizationId } from 'selectors/organizations.js';
-import { selectCategoriesStatus, selectCategories } from 'selectors/categories.js';
 
-import LoadingView from 'components/utils/loading-view';
+import Spinner from 'components/utils/spinner';
 import TableBody from './table-body';
+
+const categoriesQuery = gql`
+  query categories($orgId: ID!) {
+    organization(id: $orgId) {
+      categories {
+        id
+        name
+        type
+      }
+    }
+  }
+`;
 
 interface StateProps {
   orgId:       number;
-  status:      Status;
-  categories?: Category[];
 }
 
-interface DispatchProps {
-  load: (orgId: number) => void;
-}
-
-type Props = StateProps & DispatchProps;
+type Props = StateProps;
 
 class CategoriesList extends React.Component<Props> {
-  loadData = ({ orgId, load }: Props) => {
-    load(orgId);
-  }
-
-  componentDidMount() {
-    this.loadData(this.props);
-  }
-
-  componentWillReceiveProps(props: Props) {
-    if (props.status === Status.Invalid) {
-      this.loadData(props);
-    }
-  }
-
   render() {
-    const { status, categories } = this.props;
-
-    if (status !== Status.Success || !categories) {
-      return <LoadingView status={ status } />;
-    }
+    const { orgId } = this.props;
 
     return (
-      <Table striped responsive hover id="categories">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Type</th>
-            <th colSpan={ 2 } />
-          </tr>
-        </thead>
-        <TableBody categories={ categories } />
-      </Table>
+      <Query query={ categoriesQuery } variables={ { orgId } }>
+        {
+          ({ loading, error, data }) => {
+            if (loading) { return <Spinner />; }
+            if (error) { return <Alert bsStyle="danger">{ error }</Alert>; }
+
+            return (
+              <Table striped responsive hover id="categories">
+                <thead>
+                  <tr>
+                    <th>Name</th>
+                    <th>Type</th>
+                    <th colSpan={ 2 } />
+                  </tr>
+                </thead>
+                <TableBody categories={ data.organization.categories } />
+              </Table>
+            );
+          }
+        }
+      </Query>
     );
   }
 }
 
 const mapState = (state: {}) => ({
-  orgId:      getCurrentOrganizationId(state),
-  status:     selectCategoriesStatus(state),
-  categories: selectCategories(state),
+  orgId: getCurrentOrganizationId(state),
 });
 
-const mapDispatch = (dispatch: Dispatch<{}>) => ({
-  load: (orgId: Number) => dispatch(loadCategories(orgId)),
-});
-
-export default connect<StateProps, DispatchProps>(mapState, mapDispatch)(CategoriesList);
+export default connect<StateProps>(mapState)(CategoriesList);
