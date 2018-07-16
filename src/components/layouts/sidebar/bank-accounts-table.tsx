5e540afd8b47135ffde855cd2a1216dcb94ba3a7
Yuri Skurikhin <yunixon@gmail.com>
2018-07-16 11:45:53 +0300

    fix sidebar

--- src/components/layouts/sidebar/bank-accounts-table.tsx
+++ src/components/layouts/sidebar/bank-accounts-table.tsx
@@ -17,8 +17,8 @@ const SidebarBankAccountsTable: React.SFC<Props> = ({ bankAccounts }) => {
 
     return (
       <tr key={ bankAccount.id }>
-        <td className="text-right">{ formatBankAccountName(bankAccount) }</td>
-        <td className={ colorClass }>{ formatMoney(bankAccount.balance) }</td>
+        <td>{ formatBankAccountName(bankAccount) }</td>
+        <td className={ `text-right ${colorClass}` }>{ formatMoney(bankAccount.balance) }</td>
       </tr>
     );
   });
@@ -27,7 +27,7 @@ const SidebarBankAccountsTable: React.SFC<Props> = ({ bankAccounts }) => {
     <Table striped responsive id="bankAccounts">
       <thead>
         <tr>
-          <th className="text-right">Account</th>
+          <th>Account</th>
           <th>Amount</th>
         </tr>
       </thead>
