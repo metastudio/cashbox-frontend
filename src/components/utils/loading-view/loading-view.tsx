2a3178e9f26b458d534f8f470c059c9d85d2b7d1
Ildar Shaynurov <shaynurov@gmail.com>
2018-08-08 15:42:26 +0300

    refact: refactor loading view for better readability

--- src/components/utils/loading-view/loading-view.tsx
+++ src/components/utils/loading-view/loading-view.tsx
@@ -12,39 +12,49 @@ interface IProps {
   children?:   React.ReactNode | ((status?: Status) => React.ReactNode);
 }
 
-const LoadingView: React.SFC<IProps> = ({ status, error, onTryAgain, children }) => {
-  let body: React.ReactNode = null;
+class LoadingView extends React.Component<IProps> {
+  public static defaultProps = {
+    error: 'Error on data loading.',
+  };
 
-  switch (status) {
-    case Status.Failure:
-      body = (
-        <Alert bsStyle="danger">
-          { error || 'Error on data loading.' }
-          { ' ' }
-          { onTryAgain && <a href="" onClick={ (e) => { e.preventDefault(); onTryAgain(); } }>Try again</a> }
-        </Alert>
-      );
-      break;
-    case Status.Success:
-      if (typeof children === 'function') {
-        body = children(status);
-      } else {
-        body = children;
-      }
-      break;
-    default:
-      body = <Spinner />;
+  private handleTryAgain = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    e.preventDefault();
+    this.props.onTryAgain && this.props.onTryAgain();
   }
 
-  return (
-    <div>
-      { body }
-    </div>
-  );
-};
+  private renderError = () => {
+    const { error, onTryAgain } = this.props;
+    return (
+      <Alert bsStyle="danger">
+        { error || 'Error on data loading.' }
+        { ' ' }
+        { onTryAgain && <a href="" onClick={ this.handleTryAgain }>Try again</a> }
+      </Alert>
+    );
+  }
 
-LoadingView.defaultProps = {
-  error: 'Error on data loading.',
-};
+  private renderChildren = () => {
+    const { children, status } = this.props;
+
+    if (typeof children === 'function') { return children(status); }
+
+    return children;
+  }
+
+  private renderSpinner = () => <Spinner />;
+
+  public render() {
+    const { status } = this.props;
+
+    switch (status) {
+      case Status.Failure:
+        return this.renderError();
+      case Status.Success:
+        return this.renderChildren();
+      default:
+        return this.renderSpinner();
+    }
+  }
+}
 
 export default LoadingView;
