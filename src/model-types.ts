26ce95e56ac46c7602784c8b97c50f91470f27c1
Ildar Shaynurov <shaynurov@gmail.com>
2018-08-08 13:02:40 +0300

    refact: add special type for ID field

--- src/model-types.ts
+++ src/model-types.ts
@@ -1,13 +1,17 @@
-export enum Status {
+type ID = number;
+
+enum Status {
   Invalid = 'INVALID',
   Pending = 'PENDING',
   Success = 'SUCCESS',
   Failure = 'FAILURE',
 }
 
-export interface IPagination {
+interface IPagination {
   current: number;
   previous?: number;
   pages: number;
   next?: number;
 }
+
+export { ID, Status, IPagination };
