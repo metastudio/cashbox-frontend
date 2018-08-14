ee44abf429b89dda20709c63da9880cb108cce03
Ildar Shaynurov <shaynurov@gmail.com>
2018-08-14 15:04:53 +0300

    feat(Date): add utility to format date for date picker input

--- src/utils/date/date.ts
+++ src/utils/date/date.ts
@@ -1,11 +1,28 @@
 import * as Moment from 'moment';
 
+/**
+ * Format date in current locale.
+ * @param date - Date object or date string which may be parsed by Moment.js.
+ */
 const formatDate = (date: Date | undefined): string => {
   return date ? Moment(date).format('L') : '';
 };
 
+/**
+ * Format date to format used by date picker input.
+ * @param date - Date object or date string which may be parsed by Moment.js.
+ */
+const formatDateValue = (date: Date | string | undefined): string => {
+  return date ? Moment(date).format('YYYY-MM-DD') : '';
+};
+
+/**
+ * Format date range with current locale.
+ * @param startDate - Date object or date string which may be parsed by Moment.js.
+ * @param endDate - Date object or date string which may be parsed by Moment.js.
+ */
 const formatDateRange = (startDate: Date | undefined, endDate: Date | undefined): string => {
   return `${formatDate(startDate)} — ${formatDate(endDate)}`;
 };
 
-export { formatDate, formatDateRange };
+export { formatDate, formatDateValue, formatDateRange };
