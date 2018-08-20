c55c59f0ae8dd0351ea99ad1b5868ee0b5b10003
Ildar Shaynurov <shaynurov@gmail.com>
2018-08-20 17:00:48 +0300

    refact(Transaction): use new component to show value on show modal instead creating form

--- src/components/transactions/show/normal.tsx
+++ src/components/transactions/show/normal.tsx
@@ -5,32 +5,24 @@ import { ITransaction } from 'services/transactions';
 import { formatDate } from 'utils/date';
 import { formatMoney } from 'utils/money';
 
-import Form, { ITransactionShowFormData } from './normal-form';
+import { HorizontalStaticField } from 'components/utils/static-field';
 
 interface IProps {
   transaction: ITransaction;
 }
 
-class ShowNormalTransaction extends React.PureComponent<IProps> {
-
-  private initialValues = (): ITransactionShowFormData => {
-    const { transaction } = this.props;
-
-    return ({
-      amount:      formatMoney(transaction.amount),
-      category:    transaction.category && transaction.category.name,
-      customer:    transaction.customer && transaction.customer.name,
-      bankAccount: transaction.bankAccount && formatBankAccountName(transaction.bankAccount),
-      comment:     transaction.comment,
-      date:        formatDate(transaction.date),
-    });
-  }
-
-  public render() {
-    return(
-      <Form initialValues={ this.initialValues() } />
-    );
-  }
-}
+const ShowNormalTransaction: React.SFC<IProps> = ({ transaction }) => (
+  <div className="form-horizontal">
+    <HorizontalStaticField label="Amount" value={ formatMoney(transaction.amount) } />
+    <HorizontalStaticField label="Category" value={ transaction.category && transaction.category.name } />
+    <HorizontalStaticField label="Customer" value={ transaction.customer && transaction.customer.name } />
+    <HorizontalStaticField
+      label="Bank account"
+      value={ transaction.bankAccount && formatBankAccountName(transaction.bankAccount) }
+    />
+    <HorizontalStaticField label="Comment" value={ transaction.comment } />
+    <HorizontalStaticField label="Date" value={ formatDate(transaction.date) } />
+  </div>
+);
 
 export default ShowNormalTransaction;
