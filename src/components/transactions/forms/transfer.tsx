7beb48f03896accbf533e79e3998d1403f62265d
Ildar Shaynurov <shaynurov@gmail.com>
2018-10-15 14:38:56 +0300

    feat(Transactions): show exchange rate field on transfer form only if from and to account are differ

--- src/components/transactions/forms/transfer.tsx
+++ src/components/transactions/forms/transfer.tsx
@@ -1,9 +1,16 @@
 import * as React from 'react';
 
 import { Alert, Form } from 'react-bootstrap';
+import { connect } from 'react-redux';
 import { Field, InjectedFormProps, reduxForm } from 'redux-form';
 
-import { ITransfer } from 'services/transactions';
+import { TRANSFER_FORM } from 'constants/forms';
+import { IBankAccount } from 'services/bank-accounts';
+import {
+  ITransfer,
+  selectTransferFormFromBankAccount,
+  selectTransferFormToBankAccount,
+} from 'services/transactions';
 
 import { HorizontalBankAccountsSelect } from 'components/bank-accounts/select-field';
 import {
@@ -12,10 +19,16 @@ import {
   HorizontalMoneyInput,
   HorizontalSubmitButton,
 } from 'components/utils/form-inputs';
+import { IGlobalState } from 'services/global-state';
 
 interface IOwnProps {
   transfer?: ITransfer;
   action:    'Create' | 'Update';
+}
+
+interface IStateProps {
+  fromBankAccount?: IBankAccount;
+  toBankAccount?:   IBankAccount;
 }
 
 interface ITransferFormData {
@@ -29,48 +42,79 @@ interface ITransferFormData {
   date?:              string;
 }
 
-type IProps = IOwnProps & InjectedFormProps<ITransferFormData, IOwnProps>;
+type IProps = IOwnProps & InjectedFormProps<ITransferFormData, IOwnProps> & IStateProps;
 
-const TransferForm: React.SFC<IProps> = ({ handleSubmit, submitting, error, action, transfer }) => {
-  const isPersisted = transfer && transfer.id;
+class TransferForm extends React.PureComponent<IProps> {
+  private isPersisted = (): boolean => {
+    const { transfer } = this.props;
 
-  return (
-    <Form horizontal onSubmit={ handleSubmit }>
-      { error && <Alert bsStyle="danger">{ error }</Alert> }
+    return !!(transfer && transfer.id);
+  }
+  private isDifferentCurrencies = (): boolean => {
+    const { fromBankAccount, toBankAccount } = this.props;
+    if (!fromBankAccount || !toBankAccount) { return false; }
 
-      <Field
-        name="toAmount"
-        label="Amount"
-        component={ HorizontalMoneyInput }
-        required
-      />
-      { isPersisted && <Field name="fromAmount" label="From Amount" component={ HorizontalMoneyInput } disabled /> }
-      <Field
-        name="fromBankAccountId"
-        component={ HorizontalBankAccountsSelect }
-        label="From"
-        disabled={ isPersisted }
-        required
-      />
-      <Field
-        name="toBankAccountId"
-        label="To"
-        component={ HorizontalBankAccountsSelect }
-        disabled={ isPersisted }
-        required
-      />
-      { !isPersisted && <Field name="exchangeRate" label="Exchange Rate" component={ HorizontalFormInput } /> }
-      { !isPersisted && <Field name="comission" label="Comission" component={ HorizontalMoneyInput } /> }
-      <Field name="comment" label="Comment" component={ HorizontalFormInput } />
-      <Field name="date" label="Date" component={ HorizontalDatePicker } required />
+    return fromBankAccount.currency !== toBankAccount.currency;
+  }
 
-      <HorizontalSubmitButton submitting={ submitting }>{ action } Transfer</HorizontalSubmitButton>
-    </Form>
-  );
-};
+  private renderExchangeRateField = (): React.ReactNode => {
+    const isPersisted      = this.isPersisted();
+    const isDiffCurrencies = this.isDifferentCurrencies();
+    if (isPersisted)       { return null; }
+    if (!isDiffCurrencies) { return null; }
+
+    return <Field name="exchangeRate" label="Exchange Rate" component={ HorizontalFormInput } />;
+  }
+
+  public render() {
+    const { handleSubmit, submitting, error, action } = this.props;
+    const isPersisted      = this.isPersisted();
+
+    return (
+      <Form horizontal onSubmit={ handleSubmit }>
+        { error && <Alert bsStyle="danger">{ error }</Alert> }
+
+        <Field
+          name="toAmount"
+          label="Amount"
+          component={ HorizontalMoneyInput }
+          required
+        />
+        { isPersisted && <Field name="fromAmount" label="From Amount" component={ HorizontalMoneyInput } disabled /> }
+        <Field
+          name="fromBankAccountId"
+          component={ HorizontalBankAccountsSelect }
+          label="From"
+          disabled={ isPersisted }
+          required
+        />
+        <Field
+          name="toBankAccountId"
+          label="To"
+          component={ HorizontalBankAccountsSelect }
+          disabled={ isPersisted }
+          required
+        />
+        { this.renderExchangeRateField() }
+        { !isPersisted && <Field name="comission" label="Comission" component={ HorizontalMoneyInput } /> }
+        <Field name="comment" label="Comment" component={ HorizontalFormInput } />
+        <Field name="date" label="Date" component={ HorizontalDatePicker } required />
+
+        <HorizontalSubmitButton submitting={ submitting }>{ action } Transfer</HorizontalSubmitButton>
+      </Form>
+    );
+  }
+}
+
+const mapState = (state: IGlobalState): IStateProps => ({
+  fromBankAccount: selectTransferFormFromBankAccount(state),
+  toBankAccount:   selectTransferFormToBankAccount(state),
+});
+
+const TransferFormContainer = connect<IStateProps, {}, IOwnProps>(mapState)(TransferForm);
 
 const ReduxTransferForm = reduxForm<ITransferFormData, IOwnProps>({
-  form: 'transferForm',
-})(TransferForm);
+  form: TRANSFER_FORM,
+})(TransferFormContainer);
 
 export { ReduxTransferForm as default, ITransferFormData };
