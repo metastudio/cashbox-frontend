63661a82e76978de9c86a52cb7b8727994c95cd5
Ildar Shaynurov <shaynurov@gmail.com>
2018-10-16 11:12:30 +0300

    refact(Invoice): rewrite reducer forinvoice form via typescript

--- src/services/redux-form/reducers.ts
+++ src/services/redux-form/reducers.ts
@@ -1,6 +1,6 @@
 import { INVOICE_FORM, TRANSFER_FORM } from 'constants/forms';
 
-import invoiceFormReducer from './invoice-form/reducer.js';
+import invoiceFormReducer from './invoice-form/reducer';
 import transferFormReducer from './transfer-form/reducer';
 
 export default {
