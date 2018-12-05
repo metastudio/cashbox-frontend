7cea5d20376e25cf676439152ae8130784d0a575
Yuri Skurikhin <yunixon@gmail.com>
2018-12-05 16:52:57 +0300

    keep statistic period

--- src/components/statistic/menu.tsx
+++ src/components/statistic/menu.tsx
@@ -2,32 +2,39 @@ import * as React from 'react';
 
 import { Nav, NavItem, PageHeader } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
+import { RouteComponentProps, withRouter } from 'react-router-dom';
 
 import { balanceStatisticPath, categoriesStatisticPath, customersStatisticPath } from 'routes';
 
-const StatisticMenu: React.SFC = () => {
-  return (
-    <>
-      <PageHeader>Charts</PageHeader>
-      <Nav bsStyle="pills" stacked>
-        <LinkContainer exact to={ balanceStatisticPath() }>
-          <NavItem eventKey={ 1 }>
-            Balance
-          </NavItem>
-        </LinkContainer>
-        <LinkContainer exact to={ categoriesStatisticPath() }>
-          <NavItem eventKey={ 1 }>
-            Categories
-          </NavItem>
-        </LinkContainer>
-        <LinkContainer exact to={ customersStatisticPath() }>
-          <NavItem eventKey={ 1 }>
-            Customers
-          </NavItem>
-        </LinkContainer>
-      </Nav>
-    </>
-  );
-};
+type IProps = RouteComponentProps<{}>;
 
-export default StatisticMenu;
+class StatisticMenu extends React.PureComponent<IProps> {
+  public render() {
+    const { location: { search } } = this.props;
+
+    return (
+      <>
+        <PageHeader>Charts</PageHeader>
+        <Nav bsStyle="pills" stacked>
+          <LinkContainer exact to={ balanceStatisticPath() }>
+            <NavItem eventKey={ 1 }>
+              Balance
+            </NavItem>
+          </LinkContainer>
+          <LinkContainer exact to={ { search, pathname: categoriesStatisticPath() } }>
+            <NavItem eventKey={ 1 }>
+              Categories
+            </NavItem>
+          </LinkContainer>
+          <LinkContainer exact to={ { search, pathname: customersStatisticPath() } }>
+            <NavItem eventKey={ 1 }>
+              Customers
+            </NavItem>
+          </LinkContainer>
+        </Nav>
+      </>
+    );
+  }
+}
+
+export default withRouter(StatisticMenu);
